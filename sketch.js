cd75c5238e5201f5e5687f7a534b7a79755af3f5
Lauren Tsao <et2676@nyu.edu>
2024-09-19 01:04:47 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,83 +12,78 @@ function setup() {
   //image(img01, 0, 0, 500, 676);
 
   let trans = 255;
+  let xcent = 250;
+  let ycent = 338;
   
-  //red rectangle
+  //white square
   push();
   strokeWeight(0)
-  fill(192, 36, 13, trans);
-  rect(158, 72, 59, 180)
+  fill(10, 50, 100, trans);
+  rect(xcent, ycent, 500, 800)
   pop()
 
-  //white triangle
+  //left rectangle
   push();
   strokeWeight(0)
-  fill(225, 212, 204, trans);
-  triangle(217, 115, 452, 37, 412, 117);
+  blendMode(OVERLAY)
+  fill(0, 255, 0, trans);
+  rect(0, 0, 50, 1352)
   pop()
 
-  //blue triangle
+  //right rectangle
   push();
   strokeWeight(0)
-  fill(84, 127, 232, trans);
-  triangle(41, 231, 105, 151, 158, 230);
-  pop()
-
-  //brown square
-  push();
-  strokeWeight(0)
-  fill(133, 40, 35, trans);
-  rect(250, 338, 130, 130)
-  pop()
-
-  //green quad
-  push();
-  strokeWeight(0)
-  fill(25, 135, 108, trans);
-  quad(348, 287, 389, 252, 424, 292, 390, 328);
-  pop()
-
-  //green rectangle
-  push();
-  strokeWeight(0)
-  fill(14, 104, 94, trans);
-  rect(0, 314, 217, 141)
-  pop()
-
-  //grey semi-circle TOP
-  push();
-  strokeWeight(0)
-  blendMode(MULTIPLY)
-  fill(73, 115, 225, trans);
-  arc(250, 0, 500, 500, 0, 180);
-  pop()
-
-  //grey semi-circle BOTTOM
-  push();
-  strokeWeight(0)
-  fill(300, 100, 125, trans);
-  arc(250, 676, 500, 500, 0, 360);
+  blendMode(OVERLAY)
+  fill(0, 255, 0, trans);
+  rect(500, 676, 59, 1352)
   pop()
 
   //blue rectangle
   push();
   strokeWeight(0)
-  fill(68, 116, 224, trans);
-  rect(0, 455, 85, 53)
+  fill(0, 0, 225, trans);
+  rect(xcent, ycent, 300, 141)
+  pop()
+
+  //quad
+  push();
+  strokeWeight(0)
+  blendMode(DIFFERENCE)
+  fill(0, 50, 225, trans);
+  quad(0, ycent, xcent, 200, 500, ycent, xcent, 480);
+  pop()
+
+  //brown square
+  push();
+  strokeWeight(0)
+  translate(250, 338)
+  rotate(45)
+  fill(250, 150, 0, trans);
+  rect(0, 0, 125, 125)
+  pop()
+
+  //semi-circle TOP
+  push();
+  strokeWeight(0)
+  blendMode(DIFFERENCE)
+  fill(150, 115, 225, trans);
+  arc(250, 0, 500, 500, 0, 180);
+  pop()
+
+  //semi-circle BOTTOM
+  push();
+  strokeWeight(0)
+  blendMode(DIFFERENCE)
+  fill(150, 115, 225, trans);
+  arc(250, 676, 500, 500, 0, 360);
   pop()
 
   //yellow rectangle
   push();
   strokeWeight(0)
-  fill(247, 187, 1, trans);
-  rect(85, 455, 57, 105)
-  pop()
-
-  //white square
-  push();
-  strokeWeight(0)
-  fill(235, 234, 232, trans);
-  rect(65, 560, 20, 20)
+  blendMode(SOFT_LIGHT)
+  fill(247, 0, 0, trans);
+  rect(xcent, ycent, 72, 100)
   pop()
 
   //yellow circle
@@ -99,6 +94,21 @@ function setup() {
   circle(width/2, height/2, 500)
   pop()
 
+ //blue triangle
+ push();
+ strokeWeight(0)
+ blendMode(OVERLAY)
+ fill(100, 0, 225, trans);
+ triangle(0, 0, 250, 338, 0, 676);
+ pop()
+
+ //blue triangle 02
+ push();
+ strokeWeight(0)
+ blendMode(OVERLAY)
+ fill(100, 0, 225, trans);
+ triangle(500, 0, 250, 338, 500, 676);
+ pop()
 
 }
 
